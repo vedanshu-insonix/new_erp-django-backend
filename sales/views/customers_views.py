74cc5e5681f95059b133e3a69f6a4b7e3d951724
SidhiPathak <sidhikumari48@gmail.com>
2023-04-13 16:54:54 +0530

    update api of customers.

--- sales/views/customers_views.py
+++ sales/views/customers_views.py
@@ -98,6 +98,10 @@ class CustomerViewSet(viewsets.ModelViewSet):
                         address_serializers = AddressSerializer(address_rec, data=address_data, context={'request': request})
                         if address_serializers.is_valid(raise_exception=True):
                             address_serializers.save()
+                    else:
+                        address_serializers = AddressSerializer(data=address_data, context={'request': request})
+                        if address_serializers.is_valid(raise_exception=True):
+                            address_serializers.save()
                 returnData = CustomerSerializer(CustomerInstance, context={'request': request})
                 return Response(returnData.data)
             return None
