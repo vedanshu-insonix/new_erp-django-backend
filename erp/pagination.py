a01edd5dd204c81918be957ba9877ec493925816
SidhiPathak <sidhikumari48@gmail.com>
2023-04-14 17:25:34 +0530

    modified pagination feature.

--- erp/pagination.py
+++ erp/pagination.py
@@ -2,8 +2,19 @@
 Django rest framework default pagination
 """
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size = 30
     page_size_query_param = 'page_size'
+    page_query_param = 'page'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'nextPageNumber': self.page.next_page_number() if self.page.has_next() else None,
+            'previousPageNumber': self.page.previous_page_number() if self.page.has_previous() else None,
+            'next': self.get_next_link() if self.page.has_next() else None,
+            'previous': self.get_previous_link() if self.page.has_previous() else None,
+            'pages': self.page.paginator.num_pages,
+            'results': data
+        })
